38452ca6a11c9a002527ddcb8738aa3e1e706a1f
Sridhar Raman Admin <sridharramanadmin@macbook-pro-2.corp.ad.fmcna.com>
2019-11-20 17:43:24 -0500

    x

--- visa-bulletin-analysis/parse-visa-bulletin.py
+++ visa-bulletin-analysis/parse-visa-bulletin.py
@@ -1,2 +1,13 @@
+import os
 
+bulletin_settings = "bulletin-settings.yml"
 
+def load_settings_yml (workspace) :
+    print ('debug message for loading ')
+    yaml_settings_file = workspace + '/' + bulletin_settings
+    print (yaml_settings_file)
+
+if __name__ == '__main__':
+    workspace = os.getcwd()
+    print ('current-working-directory', workspace)
+    load_settings_yml(workspace)
